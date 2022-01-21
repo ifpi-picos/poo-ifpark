90bde26a0ec43c2ed20ce285e4bc24e0dd034e87
Ítalo Paixão Gomes <88109422+Italopax@users.noreply.github.com>
2022-01-21 20:46:36 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -3,4 +3,7 @@ public class Estacionamento {
   private int valorHora;
   private int vagas;
   private int vagasOcupadas;
+  private Endereco endereco;
+  private List<CadastroVeiculos> cadastroVeiculos = new ArrayList<>();
+
 }
