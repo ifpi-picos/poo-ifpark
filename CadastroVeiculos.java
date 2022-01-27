c1826d193e0d0db6851a4d13768739662f3ad493
Ítalo Paixão Gomes <88109422+Italopax@users.noreply.github.com>
2022-01-27 11:06:48 -0300

    Deletando classe CadastroVeiculos

--- CadastroVeiculos.java
+++
@@ -1,9 +0,0 @@
-import java.util.Calendar;
-import java.util.spi.CalendarDataProvider;
-
-public class CadastroVeiculos {
-  private String placa;
-  private String dataHoraEntrada;
-  private String dataHoraSaida;
-  private float valorTotal;
-}
