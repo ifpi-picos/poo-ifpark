25f312e7b27d089cbc95f9f0fab846b4a0062957
Ítalo Paixão Gomes <88109422+Italopax@users.noreply.github.com>
2022-01-27 11:07:26 -0300

    Deletando classe Estacionamento

--- Estacionamento.java
+++
@@ -1,9 +0,0 @@
-public class Estacionamento {
-  private String nome;
-  private int valorHora;
-  private int vagas;
-  private int vagasOcupadas;
-  private Endereco endereco;
-  private List<CadastroVeiculos> cadastroVeiculos = new ArrayList<>();
-
-}
